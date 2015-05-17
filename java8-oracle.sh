4168f5a9d7de1a88456cf6063d2ad02e4648830d
Wesley Tanaka <wtanaka@yahoo.com>
2015-05-17 09:14:31 -1000

    Add sudo, push to java8-oracle.sh

--- java8-oracle.sh
+++ java8-oracle.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # Copyright (C) 2015 Wesley Tanaka
-docker build \
+# Creates a docker image for Oracle Java 8
+sudo docker build \
    -t wtanaka/java8-oracle:`git describe --always | tr :A-Z -a-z` \
-   java8-oracle
+   java8-oracle/
+sudo docker push wtanaka/java8-oracle
